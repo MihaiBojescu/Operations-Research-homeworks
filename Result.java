021db8a66f4ed2fc65a05d549c57fa2255093eb8
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-12-03 00:49:36 +0200

    Homework 3.1: Add result class

---
+++ Result.java
@@ -0,0 +1,9 @@
+public class Result {
+    double[] solution;
+    double objectiveValue;
+
+    public Result(double[] solution, double objectiveValue) {
+        this.solution = solution;
+        this.objectiveValue = objectiveValue;
+    }
+}
