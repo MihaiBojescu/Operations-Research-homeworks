3d17056085c41691e3d93d731235678fb36144cd
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-12-07 17:53:34 +0200

    Homework 3.1: Add two-phase solver stub, to be implemented and used as heuristic

---
+++ TwoPhaseSolver.java
@@ -0,0 +1,8 @@
+public class TwoPhaseSolver implements Solver {
+    @Override
+    public Result run(Problem problem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
